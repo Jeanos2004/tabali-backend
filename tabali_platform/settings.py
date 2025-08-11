9317dd62ee1ef37acdb71f33696b1df31423b0de
Ouamouno <jeankelouaouamoun71@gmail.com>
2025-08-11 10:41:14 +0000

    modification lie

--- tabali_platform/settings.py
+++ tabali_platform/settings.py
@@ -112,11 +112,12 @@ WSGI_APPLICATION = 'tabali_platform.wsgi.application'
 # ==============================================================================
 
 # Utilisation de SQLite par défaut pour le développement
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # ==============================================================================
@@ -247,6 +248,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
